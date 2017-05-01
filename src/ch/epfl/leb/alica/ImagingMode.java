7a0a1a7bf678ffcb3444706679fe22992dac1c8d
Marcel Stefko <marcel.stefko@epfl.ch>
2017-05-01 12:10:21 +0200

    Added ImagingMode javadoc

--- src/ch/epfl/leb/alica/ImagingMode.java
+++ src/ch/epfl/leb/alica/ImagingMode.java
@@ -20,8 +20,12 @@
 package ch.epfl.leb.alica;
 
 /**
- *
- * @author stefko
+ * Possible ways for the plugin to grab images from micromanager.
+ *  - GRAB_FROM_CORE: Query directly the MMCore getLastImage() method
+ *  - LIVE: Get images from the Datastore associated with live() mode.
+ *  - NEXT_ACQUISITION: Get images from the Datastore which is associated
+ *  with the next acquisition that will be started.
+ * @author Marcel Stefko
  */
 public enum ImagingMode {
     GRAB_FROM_CORE,
