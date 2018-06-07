fae98e64a4fffb04b3c9ed0919d1a2f7eb8ba4b8
kmdouglass <kyle.m.douglass@gmail.com>
2018-06-07 16:55:06 +0200

    Bump patch version number.

--- src/main/java/ch/epfl/leb/alica/AlicaPlugin.java
+++ src/main/java/ch/epfl/leb/alica/AlicaPlugin.java
@@ -31,7 +31,7 @@ import org.scijava.plugin.SciJavaPlugin;
 @Plugin(type = MenuPlugin.class)
 public class AlicaPlugin implements MenuPlugin, SciJavaPlugin {
     
-    private final String VERSION = "0.3.2-SNAPSHOT";
+    private final String VERSION = "0.3.2";
     private final String COPYRIGHT = "The Laboratory of Experimental " +
                                      "Biophysics, Ecole Polytechnique " +
                                      "Federale de Lausanne (EPFL), Switzerland";
